5cf64022456bc03eb2595aea50425fd8dfa1aa10
Grnetsky <1101767893@qq.com>
2023-07-29 20:36:39 +0800

    测试issue_template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-export function clone(target){
-    const a = 1
-    Array.from('abc')
-    return target+a
+import { type } from "./type.js";
+
+
+Array.from('abc') // ['a', 'b', 'c']
+
+export function clone(source) {
+    const t = type(source);
+    if (t !== "object" && t !== "array") {
+        return source;
+    }
+
+    let target;
+
+    if (t === "object") {
+        target = {};
+        for (let i in source) {
+            if (source.hasOwnProperty(i)) {
+                target[i] = clone(source[i]); // 注意这里
+            }
+        }
+    } else {
+        target = [];
+        for (let i = 0; i < source.length; i++) {
+            target[i] = clone(source[i]); // 注意这里
+        }
+    }
+
+    return target;
 }
